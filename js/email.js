ae86b206d93d9d15368cb4e5ab27924086a51568
francisco <ecoli404@icloud.com>
2020-11-01 00:39:54 -0600

    Agregando javascript

---
+++ js/email.js
@@ -0,0 +1,5 @@
+function fn1() {
+    var str = document.getElementById("emailme").value;
+    alert("No te llegará ningún correo ni nada, Amaranto, pero se ve bonito uwu. Ah, tu correo es: "+str)
+    document.getElementById("emailme").value = "";
+}
